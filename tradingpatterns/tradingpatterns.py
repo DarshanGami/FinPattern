25c3df3229d097532ae635dd8e1893c48ad28ecf
Gami Darshan <gamidarshan633@gmail.com>
2025-11-18 23:33:01 +0530

    Remove detect_trendline function
    Removed the detect_trendline function and its associated logic.

--- tradingpatterns/tradingpatterns.py
+++ tradingpatterns/tradingpatterns.py
@@ -130,36 +130,6 @@ def detect_double_top_bottom(df, window=3, threshold=0.05):
     df.loc[mask_double_bottom, 'double_pattern'] = 'Double Bottom'
     return df
 
-def detect_trendline(df, window=2):
-    # Define the rolling window
-    roll_window = window
-    # Create new columns for the linear regression slope and y-intercept
-    df['slope'] = np.nan
-    df['intercept'] = np.nan
-
-    for i in range(window, len(df)):
-        x = np.array(range(i-window, i))
-        y = df['Close'][i-window:i]
-        A = np.vstack([x, np.ones(len(x))]).T
-        m, c = np.linalg.lstsq(A, y, rcond=None)[0]
-        df.at[df.index[i], 'slope'] = m
-        df.at[df.index[i], 'intercept'] = c
-
-    # Create a boolean mask for trendline support
-    mask_support = df['slope'] > 0
-
-    # Create a boolean mask for trendline resistance
-    mask_resistance = df['slope'] < 0
-
-    # Create new columns for trendline support and resistance
-    df['support'] = np.nan
-    df['resistance'] = np.nan
-
-    # Populate the new columns using the boolean masks
-    df.loc[mask_support, 'support'] = df['Close'] * df['slope'] + df['intercept']
-    df.loc[mask_resistance, 'resistance'] = df['Close'] * df['slope'] + df['intercept']
-    return df
-
 def find_pivots(df):
     # Calculate differences between consecutive highs and lows
     high_diffs = df['high'].diff()
